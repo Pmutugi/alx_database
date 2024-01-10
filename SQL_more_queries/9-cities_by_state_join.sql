6cc403ac02bf1d00acbf35f17bf5edd2425b586a
sparks <spmutugi@gmail.com>
2024-01-11 02:12:14 +0300

    inner join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Active: 1704829323945@@127.0.0.1@3306@hbtn_0d_usa
----using the inner join to join cities
+-- using the inner join to join cities
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id = states.id
