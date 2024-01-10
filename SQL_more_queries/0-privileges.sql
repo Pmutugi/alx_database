a074f4685e035d78b30d975483a891a637d9a89f
sparks <spmutugi@gmail.com>
2024-01-10 23:44:26 +0300

    GRANTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- show grant to locahost.
-SHOW GRANTS FOR USER 'user_0d_1'@'localhost';
-SHOW GRANTS FOR USER 'user_0d_2'@'localhost';
+SHOW GRANTS FOR  'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
